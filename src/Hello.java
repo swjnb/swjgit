a0ad2cc4cc9b481f275d15c37d2d2b013fde3891
swjnb <swjnb123456@hotmail.com>
2019-10-13 12:01:34 +0800

    这是分支第一次提交

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("这是分支添加的内容");
+    }
+}
